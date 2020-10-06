84757241938a97a69b8399a6fa4c0d448ab2056c
JiEun Lee <39111959+eazisilver@users.noreply.github.com>
2020-10-06 16:38:15 +0900

    Create 1225. 암호생성기.java

---
+++ Problems - SWEA/1225. 암호생성기.java
@@ -0,0 +1,43 @@
+/*
+ *
+ *
+ */
+ 
+import java.util.*;
+class Solution
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        for(int tc=1; tc<=10; tc++){
+            sc.nextInt();
+            sc.nextLine();
+            String str = sc.nextLine();
+            ArrayList<String> list = new ArrayList<>(Arrays.asList(str.split(" ")));
+
+            boolean flag = true;
+            while(flag){
+                for(int i=1;i<=5;i++){
+                    int x = Integer.parseInt(list.get(0))-i;
+                    if(x <= 0){
+                        x = 0;
+                    }
+                    list.remove(0);
+                    
+                    list.add(Integer.toString(x));
+                    if(x == 0){
+                        flag = false;
+                        break;
+                    }
+                }
+            }
+            String result = "";
+            for(String s: list){
+                result += s + " ";
+            }
+            System.out.println("#" + tc + " " + result);
+        }
+        sc.close();
+    }
+}
