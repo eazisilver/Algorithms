d3d43112306997f951a2dcf2b1f8f4ed22b44be4
JiEun Lee <39111959+eazisilver@users.noreply.github.com>
2020-10-06 10:40:31 +0900

    Update and rename 1228. [SW 문제해결 기본] 8일차 - 암호문1.java to 1228. 암호문1.java

---
+++ Problems - SWEA/1228. 암호문1.java
@@ -0,0 +1,49 @@
+/*
+ * eazisilver@GitHub <jieunlee.me@gmail.com>
+ * SW Expert Academy 1228
+ * ArrayList 삽입 이용
+ */
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		for(int tc = 1; tc <= 10; tc++){   
+            int n       = sc.nextInt();
+            sc.nextLine();
+            String pw   = sc.nextLine();
+            int cmdCnt  = sc.nextInt();
+            sc.nextLine();
+            String cmd  = sc.nextLine();
+            
+            ArrayList<String> pwList = new ArrayList<>(Arrays.asList(pw.split(" ")));
+            ArrayList<String> cmdList = new ArrayList<>(Arrays.asList(cmd.split("I ")));
+
+            for(int i=1;i<=cmdCnt; i++){
+                String[] temp = cmdList.get(i).split(" ");
+                int x = Integer.parseInt(temp[0]);
+                int y = Integer.parseInt(temp[1]);
+                if(x >= n){
+                    continue;
+                }
+                if(n-x < y){
+                    y = n-x;
+                }
+                for(int j=2; j<temp.length;j++){
+                    pwList.add(x, temp[j]);
+                    x++;
+                }
+            }
+            String s = "";
+            for(int i=0;i<10;i++){
+                s += pwList.get(i) + " ";
+            }
+            System.out.println("#" + tc + " " + s);
+        }
+        sc.close();
+	}
+}
