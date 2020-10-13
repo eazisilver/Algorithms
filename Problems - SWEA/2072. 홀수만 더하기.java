976e6f0a02b4b1e5e1521936aef97e43334905f0
JiEun Lee <39111959+eazisilver@users.noreply.github.com>
2020-10-14 08:54:29 +0900

    Create 2072. 홀수만 더하기.java

---
+++ Problems - SWEA/2072. 홀수만 더하기.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class SWEA2072 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+        sc.nextLine();
+        for(int tc =1; tc<=T; tc++){
+            int odd = 0;
+            String s = sc.nextLine();
+            String[] nums = s.split(" ");
+            
+            for(int i=0;i<nums.length; i++){
+                if(Integer.parseInt(nums[i])%2 != 0){
+                    odd += Integer.parseInt(nums[i]);
+                }
+            }
+            System.out.println("#" + tc + " " + odd);
+        }
+        sc.close();
+    }
+}
