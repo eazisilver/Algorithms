d1a1a6d819cb6df2ac445856111d5914525b5d70
JiEun Lee <39111959+eazisilver@users.noreply.github.com>
2020-10-14 08:58:12 +0900

    Create 2070. 큰 놈, 작은 놈, 같은 놈.java

---
+++ Problems - SWEA/2070. 큰 놈, 작은 놈, 같은 놈.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class SWEA2070 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        sc.nextLine(); 
+
+        for(int tc =1; tc<=T; tc++){
+            String s = sc.nextLine();
+            String[] nums = s.split(" ");
+            String answer = NumCompare(nums);
+            //
+            //
+            //
+            System.out.println("#" + tc + " " + answer);
+        }
+        sc.close();
+    }
+    private static String NumCompare(String[] nums){
+        int a = Integer.parseInt(nums[0]);
+        int b = Integer.parseInt(nums[1]);
+        int c = a-b;
+        if(c < 0){
+            c = -1;
+        }else if(c > 0){
+            c = 1;
+        }else{
+            c = 0;
+        }
+        switch(c){
+            case -1: return "<";
+            case  0: return "=";
+            case  1: return ">";
+        }
+        return "";
+    }
+}
