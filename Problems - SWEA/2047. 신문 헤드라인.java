cace4338fbc6095898ad017382b2f8899fe84f44
JiEun Lee <39111959+eazisilver@users.noreply.github.com>
2020-10-14 13:17:58 +0900

    Create 2047. 신문 헤드라인.java

---
+++ Problems - SWEA/2047. 신문 헤드라인.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class SWEA2047 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        System.out.println(s.toUpperCase());
+        sc.close();
+    }
+}
