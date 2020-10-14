c728d2f3a4a9e1fea3ad1909ccd70db0cd82b687
JiEun Lee <39111959+eazisilver@users.noreply.github.com>
2020-10-14 10:45:21 +0900

    Create 2063. 중간값 찾기.java

---
+++ Problems - SWEA/2063. 중간값 찾기.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class SWEA2063 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+        String s = sc.nextLine();
+        int[] nums = Arrays.asList(s.split(" ")).stream().mapToInt(Integer::parseInt).toArray();
+        int answer = MiddleNumber(n, nums);
+        System.out.println(answer);
+        sc.close();
+    }
+    private static int MiddleNumber(int n, int[] nums){
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                if(nums[i]>nums[j]){
+                    int temp = 0;
+                    temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                }
+            }
+        }
+        return nums[n/2];
+    }
+}
